02bcf4e70358e926d5f17240503c04bb423543a6
bonnyyadav22 <bonnyyadav22@gmail.com>
2026-03-10 09:24:05 +0530

    UC8: Implemented HashMap pattern storage and render function

---
+++ UC8_OOPS_Banner.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8_OOPS_Banner {
+
+    // Function to create character pattern map
+    public static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return patternMap;
+    }
+
+    // Function to render banner
+    public static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        for (int i = 0; i < 7; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append("   ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        String word = "OOPS";
+
+        renderBanner(word, patternMap);
+    }
+}
